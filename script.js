7c691f21a630b81184ee736e3998b9487547ef27
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-09-17 21:54:49 +0800

    feat: add basic functions for operations

---
+++ script.js
@@ -0,0 +1,34 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(firstNum, secondNum, operator) {
+
+    switch(operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+    }
+}
