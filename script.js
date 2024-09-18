38a4beca7ebea69f6a7f64169da83d1a2c491cb1
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-09-18 13:11:56 +0800

    feat: add listener to button clicked and decide content display

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ function operate(firstNum, secondNum, operator) {
             return divide(firstNum, secondNum);
     }
 }
+
+const display = document.querySelector("#calc-screen");
+const buttonClicked = document.querySelector("#calc-body");
+
+buttonClicked.addEventListener("click", (e) => {
+    
+    const numberID = e.target.id;
+
+    switch(numberID) {
+        case 'zeroBtn':
+            display.textContent = '0';
+            break;
+        case 'oneBtn':
+            display.textContent = '1';
+            break;
+        case 'twoBtn':
+            display.textContent = '2';
+            break;
+        case 'threeBtn':
+            display.textContent = '3';
+            break;
+        case 'fourBtn':
+            display.textContent = '4';
+            break;
+        case 'fiveBtn':
+            display.textContent = '5';
+            break;
+        case 'sixBtn':
+            display.textContent = '6';
+            break;
+        case 'sevenBtn':
+            display.textContent = '7';
+            break;
+        case 'eightBtn':
+            display.textContent = '8';
+            break;
+        case 'nineBtn':
+            display.textContent = '9';
+            break;
+    }
+});
